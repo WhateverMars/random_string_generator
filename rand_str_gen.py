66935125631f0e9ad6588635a621016d7567a449
David Kelly <davekelly1224@gmail.com>
2021-10-11 13:52:48 +0100

    Added error handling to rand_str_gen.py

--- rand_str_gen.py
+++ rand_str_gen.py
@@ -1,12 +1,23 @@
 import random
 
-def random_string(size):
-    rand_str = ''
-    for i in range(size):
-        rand_int = random.randint(97,122)
-        if random.randint(0,1):
-            rand_int -= 32
-        rand_str += chr(rand_int)
-    return rand_str
+# This function takes an int as input and outputs 
+# a random string of that length
 
-print(random_string(10))
+def random_string(size):
+    
+    try:
+        if size <= 0:
+            return 'Size must be a positive integer'
+        rand_str = ''
+        for i in range(size):
+            rand_int = random.randint(97,122)
+            if random.randint(0,1):
+                rand_int -= 32
+            rand_str += chr(rand_int)
+        return rand_str
+    
+    except TypeError:
+        return 'Error: Usage random_string(Size) where size is given as a positve integer'
+
+# print statement used for testing
+#print(random_string(10))
