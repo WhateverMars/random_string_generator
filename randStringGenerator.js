01693ea859857dbfcb71acb5027fd6715d756ded
David Kelly <davekelly1224@gmail.com>
2021-11-26 12:21:30 +0000

    user can now input required string length on page. Result is now output underneath instead of in log

--- randStringGenerator.js
+++ randStringGenerator.js
@@ -9,16 +9,34 @@ function generatedChar(){
     return Math.floor(Math.random() * 26) + 97;
 } 
     
-let generatedString = [];
+function generatedString(length){
+
+    let generatedString = [];
     
-for(i =0; i<length; i++){
-    let new_char = generatedChar();
+    for(i =0; i<length; i++){
+        let new_char = generatedChar();
         
-    generatedString.push(String.fromCharCode(new_char));
+        generatedString.push(String.fromCharCode(new_char));
         
+    }
+
+    let word = generatedString.join('');
+    
+    
+    console.log(word);
+    return word
 }
 
-let word = generatedString.join('');
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    console.log('DOMLoaded')
+    document.querySelector('#goBtn').addEventListener('click', () => {
+        console.log('Submitted')
+        let stringLength = document.querySelector('#stringLength').value
+        console.log(stringLength)
     
-    
-console.log(word);
+        document.querySelector('#result').innerHTML = generatedString(stringLength)
+
+    }) 
+})
